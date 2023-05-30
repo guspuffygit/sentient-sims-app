9e19f6c0e7973f481dd93e180e9ac977706a9b73
guspuffygit <guspuffy@gmail.com>
2023-05-30 09:35:42 -0400

    fix linting issues with unused variables

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,7 +7,6 @@ import { LoadingButton } from '@mui/lab';
 import icon from '../../assets/icon.png';
 
 function Hello() {
-  const [sendingLogs, setSendingLogs] = useState(false);
   const [openAIStatus, setOpenAIStatus] = useState({
     status: '',
     loading: false,
@@ -38,12 +37,6 @@ function Hello() {
     testOpenAI();
   }, []);
 
-  const sendLogs = async () => {
-    setSendingLogs(true);
-    await fetch('http://localhost:25148/send-logs');
-    setSendingLogs(false);
-  };
-
   return (
     <div>
       <div className="Hello">
@@ -62,15 +55,6 @@ function Hello() {
       >
         Test Open AI
       </LoadingButton>
-      {/* <LoadingButton
-        loading={sendingLogs}
-        sx={{ marginRight: 2 }}
-        color="secondary"
-        variant="outlined"
-        onClick={sendLogs}
-      >
-        Send Logs to Gus
-      </LoadingButton> */}
     </div>
   );
 }
