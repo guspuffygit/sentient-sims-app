0653889236b19989768e5dbf9b39b067f3667d95
guspuffygit <guspuffy@gmail.com>
2025-02-24 08:48:22 -0500

    saving

--- src/main/sentient-sims/descriptions/allTraits.ts
+++ src/main/sentient-sims/descriptions/allTraits.ts
@@ -1,12 +1,6 @@
 import { TraitDescription } from '../models/TraitDescription';
-import { attractionPreferenceDescriptions } from './attractionPreferenceDescriptions';
-import { gameplayObjectPreferenceDescriptions } from './gameplayObjectPreferenceDescriptions';
-import { preferenceDescriptions } from './preferenceDescriptions';
 import { traitDescriptions } from './traitDescriptions';
 
 export const allTraits: Record<string, TraitDescription> = {
-  ...attractionPreferenceDescriptions,
-  ...gameplayObjectPreferenceDescriptions,
-  ...preferenceDescriptions,
   ...traitDescriptions,
 };
