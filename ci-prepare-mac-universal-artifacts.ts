a1cbbba8ff4783ad39ae478678511ee98b5be7ef
guspuffygit <guspuffy@gmail.com>
2025-06-06 11:47:34 -0400

    Skip existing

--- ci-prepare-mac-universal-artifacts.ts
+++ ci-prepare-mac-universal-artifacts.ts
@@ -13,10 +13,10 @@ const startTime = Date.now();
   while (Date.now() - startTime < timeout) {
     try {
       execSync(
-        `gh release download "v${VERSION}" --repo ${GITHUB_REPOSITORY} --pattern "SentientSims-${VERSION}-mac.zip" --dir .`,
+        `gh release download "v${VERSION}" --repo ${GITHUB_REPOSITORY} --pattern "SentientSims-${VERSION}-mac.zip" --skip-existing --dir .`,
       );
       execSync(
-        `gh release download v${VERSION} --repo ${GITHUB_REPOSITORY} --pattern "SentientSims-${VERSION}-arm64-mac.zip" --dir .`,
+        `gh release download v${VERSION} --repo ${GITHUB_REPOSITORY} --pattern "SentientSims-${VERSION}-arm64-mac.zip" --skip-existing --dir .`,
       );
       execSync(`unzip -q SentientSims-${VERSION}-arm64-mac.zip -d mac-arm64`);
       execSync(`unzip -q SentientSims-${VERSION}-mac.zip -d mac`);
