ac1ee2857fd3471dbb6d14de59189625c828fd81
guspuffygit <guspuffy@gmail.com>
2025-06-10 09:44:05 -0400

    saving

--- src/main/sentient-sims/db/migrations.ts
+++ src/main/sentient-sims/db/migrations.ts
@@ -102,6 +102,10 @@ export const migrations: Map<string, DbMigrationSql> = new Map(
       ALTER TABLE participant
       ADD COLUMN name TEXT;
     `,
+    '007-add-memory-game-timestamp': `
+      ALTER TABLE memory
+      ADD COLUMN GAME_TIMESTAMP NOT NULL INTEGER DEFAULT 0;
+    `,
   }),
 );
 
