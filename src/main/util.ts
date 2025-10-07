3b3639b912e4732bfdc7c7234c22410940b46f94
guspuffygit <guspuffy@gmail.com>
2025-10-06 21:09:17 -0400

    savin

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,14 +1,22 @@
 import { URL } from 'url';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import log from 'electron-log';
 
-export function resolveHtmlPath(htmlFileName: string) {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function resolveHtmlPath(htmlFileName: string): string {
   if (process.env.NODE_ENV === 'development') {
     const port = process.env.PORT || 1212;
     const url = new URL(`http://localhost:${port}`);
     url.pathname = htmlFileName;
     return url.href;
   }
-  log.info(`HTML PATH: ${`file://${path.resolve(process.cwd(), '../renderer/', htmlFileName)}`}`);
-  return `file://${path.resolve(process.cwd(), '../renderer/', htmlFileName)}`;
+
+  const resolvedPath = path.resolve(__dirname, '../renderer/', htmlFileName);
+  const fileUrl = `file://${resolvedPath}`;
+
+  log.info(`Resolved HTML Path: ${fileUrl}`);
+  return fileUrl;
 }
