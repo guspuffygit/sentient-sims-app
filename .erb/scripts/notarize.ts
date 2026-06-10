5a86263a4bd39a9598354c66ededd841c713b9d8
guspuffygit <guspuffy@gmail.com>
2026-06-10 06:52:23 -0400

    Upgrade to electron 41.7.2

--- .erb/scripts/notarize.ts
+++ .erb/scripts/notarize.ts
@@ -1,5 +1,4 @@
 import { notarize } from '@electron/notarize';
-import packageJson from '../../package.json' with { type: 'json' };
 
 export default async function notarizeMacos(context: any) {
   const { electronPlatformName, appOutDir } = context;
@@ -12,21 +11,21 @@ export default async function notarizeMacos(context: any) {
     return;
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
+  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env && 'APPLE_TEAM_ID' in process.env)) {
+    console.warn('Skipping notarizing step. APPLE_ID, APPLE_ID_PASS, and APPLE_TEAM_ID env variables must be set');
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASS) {
-    throw new Error('APPLE_ID and APPLE_ID_PASS environment must be specified');
+  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASS || !process.env.APPLE_TEAM_ID) {
+    throw new Error('APPLE_ID, APPLE_ID_PASS, and APPLE_TEAM_ID environment must be specified');
   }
 
   await notarize({
-    appBundleId: packageJson.build.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
+    teamId: process.env.APPLE_TEAM_ID,
   });
 }
